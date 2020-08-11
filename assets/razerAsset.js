4cef4e85a9fcf0a3072390ed694930430a3fd04f
kshilingburg <kshill1990@gmail.com>
2020-08-11 15:05:36 -0600

    Started framework for assets page.

---
+++ assets/razerAsset.js
@@ -0,0 +1,8 @@
+/* This asset file is for choosing a filter and
+verifying that it is filtering out the product page 
+to only have the specific product with its specifications */
+
+module.exports = (pageObject, TBD) => {
+    pageObject
+    
+}

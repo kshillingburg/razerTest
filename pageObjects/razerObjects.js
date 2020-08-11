05d941be47723a72d5413581ac877b0085fa9c87
kshilingburg <kshill1990@gmail.com>
2020-08-11 16:31:27 -0600

    added more elements and custom commands that will most likely be used.

--- pageObjects/razerObjects.js
+++ pageObjects/razerObjects.js
@@ -1,4 +1,5 @@
 var razerCommands ={
+    //Custom command that adds a filter
     clickFilter: function(filterName) {
         this.api.useXpath()
         this.click(`//span/span[text()="${filterName}"]`)
@@ -6,10 +7,16 @@ var razerCommands ={
         this.api.useCss()
         return this
     },
-    closeFilter: function(num) {
-        this.api.useXpath()
-        this.click('//mat-icon[contains(text(),"close")]')[num]
-        this.api.useCss()
+    //Custom command to check products match filter
+    filterMatch: function(arrayName, filterName) {
+        this.arrayName.forEach(product => {
+            this.assert.containsText(product, filterName)
+        }) 
+    },
+    //Custom command to verify that filters were cleared.
+    filtersCleared: function() {
+        this.click('@clearBtn')
+        this.assert.containsText('@productNumber', '17 products')
         return this
     }
 }
@@ -18,8 +25,17 @@ module.exports = {
     url: 'https://www.razer.com/shop/pc/gaming-laptop',
     commands: [razerCommands],
     elements: {
+        titleElement: '[alt="Razer Logo"]',
         clearBtn: {
-            selector: ('//a[contains(text(),"Clear All")]'),
+            selector: '//a[contains(text(),"Clear All")]',
+            locateStrategy: 'xpath'
+        },
+        closeBtn: {
+            selector: '//mat-icon[contains(text(),"close")]',
+            locateStrategy: 'xpath'
+        },
+        productNumber: {
+            selector: '//div[3]/p',
             locateStrategy: 'xpath'
         },
         

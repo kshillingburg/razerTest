4d9e2a809d4f423f33eae3e1557672c3f7af831a
kshilingburg <kshill1990@gmail.com>
2020-08-14 15:46:31 -0600

    Refined the custom command for individual filters

--- pageObjects/razerObjects.js
+++ pageObjects/razerObjects.js
@@ -1,11 +1,45 @@
 var razerCommands ={
-    //Custom command that adds a filter
-    clickFilter: function(filterName) {
-        this.api.useXpath()
-        this.click(`//span/span[text()="${filterName}"]`)
-        this.waitForElementVisible(`//mat-chip/span[text()="${filterName}"]`)
-        this.api.useCss()
+    loadProductPage: function() {
+        this.click('@agreeBtn')
+        this.click('@navProducts1')
+        this.click('@navProducts2')
+        this.waitForElementVisible('@shopPageTitle')
+        this.click('@shopLaptops')
         return this
+    },
+    //Custom command that adds a filter
+    clickFilter: function(dropFilterName,filterName) {
+        var self = this
+        this.click(dropFilterName)
+        this.pause(500)
+        this.api.useXpath()
+        this.click(`//span[contains(text(),"${filterName}")]`) 
+        this.api.useCss()    
+        this.pause(3000)
+        this.api.element("css selector",'.modal-content', function(visible) {       
+            /* During the first few filterings there is a 
+            popup that blocks the page, this will cause an 
+            error message in the code, but it is unavoidable.*/ 
+            if(visible.status ===0) {
+                self.click('i.icon-close')
+                self.pause(500)
+            } 
+            else {
+                if(filterName=== "Black "){
+                    filterName = "Black"
+                    self.api.useXpath()
+                    self.waitForElementVisible(`//mat-chip/span[(text()="${filterName}")]`)
+                    self.api.useCss()
+                }
+                else {
+                    self.api.useXpath()
+                    self.waitForElementVisible(`//mat-chip/span[(text()="${filterName}")]`)
+                    self.api.useCss()
+                }
+            }
+        })
+        return this
+ 
     },
     //Custom command to check products match filter
     filterMatch: function(arrayName, filterName) {
@@ -22,10 +56,24 @@ var razerCommands ={
 }
 
 module.exports = {
-    url: 'https://www.razer.com/shop/pc/gaming-laptop',
+    url: 'https://www.razer.com/',
     commands: [razerCommands],
     elements: {
         titleElement: '[alt="Razer Logo"]',
+        agreeBtn: '.button-primary-outline-green',
+        navProducts1: {
+            selector:'//span[contains(text(), "Products")]',
+            locateStrategy: 'xpath'
+        },
+        navProducts2: {
+            selector: '//span[contains(text(),"PC Accessories")]',
+            locateStrategy: 'xpath'
+        },
+        shopPageTitle: {
+            selector: '//h1[contains(text(),"PC")]',
+            locateStrategy: 'xpath'
+        },
+        shopLaptops: '[href="/shop/pc/gaming-laptops"]',
         clearBtn: {
             selector: '//a[contains(text(),"Clear All")]',
             locateStrategy: 'xpath'
@@ -36,6 +84,40 @@ module.exports = {
         },
         productNumber: {
             selector: '//div[3]/p',
+            locateStrategy: 'xpath'
+        },
+        popUpClose: '.icon-close',
+        //Filter drop down titles
+        dropScreenSize: {
+            selector: '//span[contains(text(), "Screen Size")]',
+            locateStrategy: 'xpath'
+        },
+        dropScreenType: {
+            selector: '//span[contains(text(), "Screen Type")]',
+            locateStrategy: 'xpath'
+        },
+        dropRefresh: {
+            selector: '//span[contains(text(), "Refresh Rate")]',
+            locateStrategy: 'xpath'
+        },
+        dropProcessor: {
+            selector: '//span[contains(text(), "Processor")]',
+            locateStrategy: 'xpath'
+        },
+        dropGraphics: {
+            selector: '//span[contains(text(), "Graphics")]',
+            locateStrategy: 'xpath'
+        },
+        dropStorage: {
+            selector: '//span[contains(text(), "Storage")]',
+            locateStrategy: 'xpath'
+        },
+        dropMemory: {
+            selector: '//span[contains(text(), "Memory")]',
+            locateStrategy: 'xpath'
+        },
+        dropColor: {
+            selector: '//span[contains(text(), "Color")]',
             locateStrategy: 'xpath'
         },
         
